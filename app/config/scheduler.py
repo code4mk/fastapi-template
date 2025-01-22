e75b81b074c33b8093631d1989f462b1714002cb
Mostafa Kamal <hiremostafa@gmail.com>
2025-01-23 02:42:11 +0600

    celery

---
+++ app/config/scheduler.py
@@ -0,0 +1,6 @@
+from fastapi_pundra.common.scheduler.schedule import Schedule
+from app.tasks.my_task import my_task
+
+schedules = [
+  Schedule().task(my_task).name('my-task-one').everyMinute()
+]
